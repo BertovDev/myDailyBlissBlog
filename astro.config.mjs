57eef89f1b0934d59f3e277ed7ebb41812ee7a43
BertovDev <bertobautista18@gmail.com>
2024-09-25 01:45:34 -0300

    Change astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: netlify()
+  output: "server",
+  adapter: netlify({
+    functionPerRoute: true,
+  }),
 });
