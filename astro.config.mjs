95029e1232328ab339de661be0f76d2f01c4d9ad
BertovDev <bertobautista18@gmail.com>
2024-09-25 01:48:15 -0300

    Change output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
+  output: "static",
   adapter: netlify({
     functionPerRoute: true,
   }),
